ecd90c1cf21f25ffafa46314c73b6aebd11a0cce
nikolay <ndikanskiy@gmail.com>
2021-05-06 20:21:11 +0300

    Sorter class added

---
+++ sorter/Sorter.java
@@ -0,0 +1,191 @@
+package sorter;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+/**
+ * Class "Sorter" have 5 different types of sort.
+ *
+ * @author Ousookie
+ * @version 1.0
+ */
+public class Sorter {
+
+    private static double[] values;
+
+    /**
+     * @param size: size of array
+     */
+    public static void setValues(int size) {
+        Random generator = new Random();
+        values = new double[size];
+        for (int i = 0; i < size; i++) {
+            values[i] = generator.nextDouble();
+        }
+    }
+
+    /**
+     * @return array of values
+     */
+    public static double[] getValues() {
+        return values;
+    }
+
+    /**
+     * @param method: different types of sort function
+     * @return execution time (except "Quick" sort function)
+     * @throws Exception ?
+     */
+    public static double timer(Callable<Void> method) throws Exception {
+        double start = System.currentTimeMillis();
+        method.call();
+        return System.currentTimeMillis() - start;
+    }
+
+    /**
+     * "Quick" sort function.
+     *
+     * @param values:    source array of values
+     * @param lowIndex:  first index of array values
+     * @param highIndex: last index of array values
+     */
+    public static void quickSort(double[] values, int lowIndex, int highIndex) {
+
+        int middleIndex = lowIndex - (lowIndex - highIndex) / 2;
+        double pivot = values[middleIndex];
+
+        int i = lowIndex;
+        int j = highIndex;
+
+        while (i < j) {
+            while (values[i] < pivot) {
+                i++;
+            }
+
+            while (values[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                double temp = values[i];
+                values[i] = values[j];
+                values[j] = temp;
+            }
+            i++;
+            j--;
+        }
+
+        if (highIndex > i) {
+            quickSort(values, i, highIndex);
+        }
+
+        if (lowIndex < j) {
+            quickSort(values, lowIndex, j);
+        }
+    }
+
+    /**
+     * @param arr:       source array of values
+     * @param lowIndex:  first index of array values
+     * @param highIndex: last index of array values
+     * @return execution time ("Quick" sort function)
+     */
+    public static double checkQuickSortTimeInMillis(double[] arr, int lowIndex, int highIndex) {
+        double start = System.currentTimeMillis();
+        quickSort(arr, lowIndex, highIndex);
+        return System.currentTimeMillis() - start;
+    }
+
+    /**
+     * "Bubble" sort function.
+     *
+     * @return null
+     */
+    public static Void checkBubbleSortTimeInMillis() {
+        for (int i = 0; i < values.length; i++) {
+            for (int j = 0; j < values.length; j++) {
+                if (values[j] > values[i]) {
+                    double temp = values[j];
+                    values[j] = values[i];
+                    values[i] = temp;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * "Insert" sort function.
+     *
+     * @return null
+     */
+    public static Void checkInsertSortTimeInMillis() {
+        for (int i = 0; i < values.length; i++) {
+            int j = i;
+            while (j > 0 && values[j - 1] > values[j]) {
+                double temp = values[j - 1];
+                values[j - 1] = values[j];
+                values[j] = temp;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @param values: source array of values
+     * @param start:  start index of array
+     * @param stop:   last index of array
+     * @return index of min value in array
+     */
+    protected static int minValueIndex(double[] values, int start, int stop) {
+        int minIndex = start;
+        for (int i = start; i <= stop; i++) {
+            if (values[i] < values[minIndex]) minIndex = i;
+        }
+        return minIndex;
+    }
+
+    /**
+     * "Selection" sort function.
+     *
+     * @return null
+     */
+    public static Void checkSelectionSortTimeInMillis() {
+        for (int i = 0; i < values.length - 1; i++) {
+            int minValueIndex = minValueIndex(values, i, values.length - 1);
+            double temp = values[i];
+            values[i] = values[minValueIndex];
+            values[minValueIndex] = temp;
+        }
+        return null;
+    }
+
+    /**
+     * "Brush" sort function.
+     *
+     * @return null
+     */
+    public static Void checkBrushSortTimeInMillis() {
+        final double fact = 1.247;
+        double step = values.length - 1;
+        while (step >= 1) {
+            for (int i = 0; i + (int) step < values.length; i++) {
+                if (values[i + (int) step] < values[i]) {
+                    double temp = values[i];
+                    values[i] = values[i + (int) step];
+                    values[i + (int) step] = temp;
+                }
+                step /= fact;
+            }
+        }
+        checkBubbleSortTimeInMillis();
+        return null;
+    }
+}
+
+
+
+
+
+
+
